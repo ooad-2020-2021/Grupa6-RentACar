1caf4602ca0422271b306edad433173121af091c
bbbasic <ebasic3@etf.unsa.ba>
2021-06-05 19:48:23 +0200

    crudVozilaZavrsen

---
+++ ImplementacijaRentAndGo/Controllers/VoziloController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ImplementacijaRentAndGo.Data;
+using ImplementacijaRentAndGo.Models;
+
+namespace ImplementacijaRentAndGo.Controllers
+{
+    public class VoziloController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VoziloController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vozilo
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Vozilo.ToListAsync());
+        }
+
+        // GET: Vozilo/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vozilo = await _context.Vozilo
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vozilo == null)
+            {
+                return NotFound();
+            }
+
+            return View(vozilo);
+        }
+
+        // GET: Vozilo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vozilo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Naziv,SlikaVozila,Gorivo,Klasa,Mjenjac,Lokacija,MaksimalanBrojPutnika,CijenaPoDanu,DatumTehnickogPregleda,BrojRegistacijskihTablica,Klima,SistemBezKljuca,AluminijskeFelge,SklopiviRetrovizori,BoardComputer,MP3Interfejs")] Vozilo vozilo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vozilo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vozilo);
+        }
+
+        // GET: Vozilo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vozilo = await _context.Vozilo.FindAsync(id);
+            if (vozilo == null)
+            {
+                return NotFound();
+            }
+            return View(vozilo);
+        }
+
+        // POST: Vozilo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Naziv,SlikaVozila,Gorivo,Klasa,Mjenjac,Lokacija,MaksimalanBrojPutnika,CijenaPoDanu,DatumTehnickogPregleda,BrojRegistacijskihTablica,Klima,SistemBezKljuca,AluminijskeFelge,SklopiviRetrovizori,BoardComputer,MP3Interfejs")] Vozilo vozilo)
+        {
+            if (id != vozilo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vozilo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VoziloExists(vozilo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vozilo);
+        }
+
+        // GET: Vozilo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vozilo = await _context.Vozilo
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vozilo == null)
+            {
+                return NotFound();
+            }
+
+            return View(vozilo);
+        }
+
+        // POST: Vozilo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vozilo = await _context.Vozilo.FindAsync(id);
+            _context.Vozilo.Remove(vozilo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VoziloExists(int id)
+        {
+            return _context.Vozilo.Any(e => e.Id == id);
+        }
+    }
+}
