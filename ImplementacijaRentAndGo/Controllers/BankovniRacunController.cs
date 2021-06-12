6d1a6d6650f1b975482f672013f470a5be808991
Nedina Muratovic <nmuratovic3@etf.unsa.ba>
2021-06-12 23:12:35 +0200

    .

---
+++ ImplementacijaRentAndGo/Controllers/BankovniRacunController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ImplementacijaRentAndGo.Data;
+using ImplementacijaRentAndGo.Models;
+
+namespace ImplementacijaRentAndGo.Controllers
+{
+    public class BankovniRacunController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BankovniRacunController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BankovniRacun
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BankovniRacun.ToListAsync());
+        }
+
+        // GET: BankovniRacun/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bankovniRacun = await _context.BankovniRacun
+                .FirstOrDefaultAsync(m => m.BrojRacuna == id);
+            if (bankovniRacun == null)
+            {
+                return NotFound();
+            }
+
+            return View(bankovniRacun);
+        }
+
+        // GET: BankovniRacun/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BankovniRacun/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BrojRacuna,DatumValidnostiKartice,ImePrezime,JMB")] BankovniRacun bankovniRacun)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bankovniRacun);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bankovniRacun);
+        }
+
+        // GET: BankovniRacun/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bankovniRacun = await _context.BankovniRacun.FindAsync(id);
+            if (bankovniRacun == null)
+            {
+                return NotFound();
+            }
+            return View(bankovniRacun);
+        }
+
+        // POST: BankovniRacun/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("BrojRacuna,DatumValidnostiKartice,ImePrezime,JMB")] BankovniRacun bankovniRacun)
+        {
+            if (id != bankovniRacun.BrojRacuna)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bankovniRacun);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BankovniRacunExists(bankovniRacun.BrojRacuna))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bankovniRacun);
+        }
+
+        // GET: BankovniRacun/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bankovniRacun = await _context.BankovniRacun
+                .FirstOrDefaultAsync(m => m.BrojRacuna == id);
+            if (bankovniRacun == null)
+            {
+                return NotFound();
+            }
+
+            return View(bankovniRacun);
+        }
+
+        // POST: BankovniRacun/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var bankovniRacun = await _context.BankovniRacun.FindAsync(id);
+            _context.BankovniRacun.Remove(bankovniRacun);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BankovniRacunExists(string id)
+        {
+            return _context.BankovniRacun.Any(e => e.BrojRacuna == id);
+        }
+    }
+}
